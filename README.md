aa2043ffc9b66839eabb061fa51a9607308a3a51
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-12-13 15:35:23 +0900

    feat: add benchmark

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-db-benchmark
+# db-benchmark
+
+Database benchmark, running database on Docker.
+
+# MySQL
+
+sysbench.
+
+| Kind | Transaction qps |  MySQL CPU | sysbench CPU | sysbench Threads |
+---- | ---- | ---- | ---- | ----
+| Select Simple | 30000qps | 4 | 4 | 8 |
+| Select Range | 21000qps | 4 | 4 | 8 |
+| Select Distinct Range | 16800qps | 4 | 4 | 8 |
+| Select Order Range | 19000qps | 4 | 4 | 8 |
+| Update Index | 19000qps | 4 | 4 | 8 |
+| Update Non-Index | 17000qps| 4 | 4 | 8 |
+| Delete Insert | 2900qps | 4 | 4 | 8 |
+| ReadWrite | 2700qps | 4 | 4 | 8 |
+
+# MariaDB
+
+# PostgreSQL
+
+# SQL Server
--- mysql/sysbench_mysql.sh
+++ mysql/sysbench_mysql.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 # * db-ps-mode: disable or auto. disable means use Dabase's prepared statement. should be disable.
 sysbench /usr/share/sysbench/oltp_common.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --table_size=100000 --db-ps-mode=disable prepare
 
-# select (30000qps)
+# select simple (30000qps)
 time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=1 --range_selects=0 run
 
 # select range (21000qps)
@@ -22,14 +22,14 @@ time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-dri
 # select order range (19000qps)
 time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=0 --range_selects=1 --range_size=100 --distinct_ranges=0 --simple_ranges=0 --sum_ranges=0 --order_ranges=1 run
 
-# update index (67000qps)
-time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=0 --range_selects=0 --index_updates=1 --non_index_updates=0 --delete_inserts=0 run
+# update index (19000qps)
+time sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=0 --range_selects=0 --index_updates=1 --non_index_updates=0 --delete_inserts=0 run
 
-# update no-index (65000qps)
-time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=0 --range_selects=0 --index_updates=0 --non_index_updates=1 --delete_inserts=0 run
+# update no-index (17000qps)
+time sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --point_selects=0 --range_selects=0 --index_updates=0 --non_index_updates=1 --delete_inserts=0 run
 
-# delete insert (3275qps)
-time sysbench /usr/share/sysbench/oltp_read_only.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --index_updates=0 --non_index_updates=0 --delete_inserts=1 run
+# delete insert (2900qps)
+time sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 --index_updates=0 --non_index_updates=0 --delete_inserts=1 run
 
 # read write (2700qps)
 time sysbench /usr/share/sysbench/oltp_read_write.lua --mysql-db=sb_test --db-driver=mysql --mysql-host=mysql --mysql-user=root --mysql-password=password --time=60 --table_size=100000 --db-ps-mode=disable --threads=8 run
